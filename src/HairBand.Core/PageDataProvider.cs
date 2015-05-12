6fd6b81760569d8420ba6e8a7ad1f297016765c3
IT Lackey <lackey_i@wustl.edu>
2015-05-12 16:06:42 -0500

    first code commit

---
+++ src/HairBand.Core/PageDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HairBand.Core
+{
+    public class PageDataProvider
+    {
+
+        public async Task<dynamic> GetPageData()
+        {
+
+            dynamic yaml = new YamlDotNet.Dynamic.DynamicYaml("");
+
+            var title = yaml.Title;
+          
+            return Task.FromResult(yaml);
+
+
+        }
+
+    }
+}
