90ca2a591959a2cd2b02b92953b0e9a115f6adeb
IT Lackey <lackey_i@wustl.edu>
2015-05-21 15:53:56 -0500

    c

--- src/HairBand/wwwroot/_references.js
+++
@@ -1,4 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../gulpfile.js" />
-/// <reference path="themes/Default/lib/bootstrap-touch-carousel/js/bootstrap-touch-carousel.js" />
-/// <reference path="themes/Default/lib/bootstrap/js/bootstrap.js" />
