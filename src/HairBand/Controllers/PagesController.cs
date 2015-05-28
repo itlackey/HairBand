6548f64d51b4ab8a85500ed46757dbf4114c70cb
IT Lackey <lackey_i@wustl.edu>
2015-05-28 15:59:26 -0500

    refactoring away from view component, wondering how to get includes to work...

--- src/HairBand/Controllers/PagesController.cs
+++ src/HairBand/Controllers/PagesController.cs
@@ -1,22 +1,30 @@
-﻿using DotLiquid;
-using DotLiquid.FileSystems;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Mvc;
-using Microsoft.Framework.OptionsModel;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.OptionsModel;
+using Microsoft.AspNet.Hosting;
+using DotLiquid;
+using DotLiquid.FileSystems;
+using System.IO;
 
-namespace HairBand.ViewComponents
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HairBand.Controllers
 {
-    public class ThemeSectionViewComponent : ViewComponent
+    public class PagesController : Controller
     {
-        public ThemeSectionViewComponent(IOptions<AppSettings> appSettings, IHostingEnvironment host)
+
+        private readonly IPageDataProvider _provider;
+
+        public PagesController(IPageDataProvider provider, IOptions<AppSettings> appSettings, IHostingEnvironment host)
         {
+            this._provider = provider;
+
             this.AppSettings = appSettings;
             this.Host = host;
+
         }
 
         public IOptions<AppSettings> AppSettings { get; private set; }
@@ -24,36 +32,46 @@ namespace HairBand.ViewComponents
 
         public string ThemePath { get; private set; }
 
-        public string GetHtml(string file)
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
         {
+            var pages = await this._provider.GetPages();
 
 
-            if (!file.EndsWith(".html"))
-                file += ".html";
+            return View(pages);
+        }
+
+
+        public async Task<IActionResult> Page(string page)
+        {
+            
+
+            ViewBag.Page = page;
+
+            var model = await this._provider.GetPageData(page);
+
+            ViewBag.PageData = model;
+
+
+            ViewBag.Content = this.GetHtml(model);
+
+
+            return View(model);
+        }
+
+        private string GetHtml(PageData page)
+        {
+            //if (!file.EndsWith(".html"))
+            //    file += ".html";
 
             this.ThemePath = this.Host.WebRootPath + "/themes/" + this.AppSettings.Options.Theme;
 
-            var templateHtml = System.IO.File.ReadAllText(ThemePath + "/" + file);
-
-            return ReplaceTokens(templateHtml);
-        }
-
-
-        public IViewComponentResult Invoke(string file)
-        {
-            var html = this.GetHtml(file);
-
-            return View("ThemeSection", html);
-
-        }
-
-        private string ReplaceTokens(string templateHtml)
-        {
-
+            var templateHtml = System.IO.File.ReadAllText(ThemePath + "/default.html"); // + file);
+       
             Template.RegisterSafeType(typeof(AppSettings), new string[] { "Title", "Theme" });
 
             Template.FileSystem = new LocalFileSystem(this.ThemePath);
-       
+
             //var context = new DotLiquid.Context();
 
             //var html = Template.FileSystem.ReadTemplateFile(context, "app_data/themes/happy/default.html");
@@ -64,10 +82,11 @@ namespace HairBand.ViewComponents
 
             var hash = Hash.FromAnonymousObject(new
             {
-                page = (ViewBag.PageData as PageData).Settings,
+                page = page.Settings,
                 site = this.AppSettings.Options,
                 theme_folder = "/themes/" + this.AppSettings.Options.Theme,
-                current_date = DateTime.Now
+                current_date = DateTime.Now,
+                content = page.Content
             });
 
 
